fe18c10eb23f604e67fcdfbc033bafd7d8b3917c
SkyLarkkx <skylarkkx@gmail.com>
2026-04-18 22:46:20 +0800

    chore(server): initialize project repository

---
+++ t-observer-server/main/java/com/edu/tobserver/TObserverApplication.java
@@ -0,0 +1,13 @@
+package com.edu.tobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TObserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TObserverApplication.class, args);
+    }
+
+}
---
+++ t-observer-server/test/java/com/edu/tobserver/TObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.tobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TObserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
