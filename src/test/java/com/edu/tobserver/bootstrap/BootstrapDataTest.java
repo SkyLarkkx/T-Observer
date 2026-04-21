c50978430c75d19e56e6be6046fb1f68959e2fc8
SkyLarkkx <skylarkkx@gmail.com>
2026-04-21 14:35:22 +0800

    feat: expand demo seed data

--- src/test/java/com/edu/tobserver/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/edu/tobserver/bootstrap/BootstrapDataTest.java
@@ -19,17 +19,26 @@ class BootstrapDataTest {
     @Test
     void shouldLoadSeedUsersAndDimensions() {
         Integer userCount = jdbcTemplate.queryForObject("select count(*) from sys_user", Integer.class);
+        Integer memberCount = jdbcTemplate.queryForObject(
+                "select count(*) from sys_user where role_code = 'MEMBER'",
+                Integer.class);
         Integer dimensionCount = jdbcTemplate.queryForObject("select count(*) from evaluation_dimension", Integer.class);
         List<String> seededUsers = jdbcTemplate.query(
-                "select username, role_code from sys_user where username in ('leader01', 'member01') order by username",
+                "select username, role_code from sys_user where username in ('admin01', 'leader01', 'member01', 'member02', 'member03') order by username",
                 (rs, rowNum) -> rs.getString("username") + ":" + rs.getString("role_code"));
         List<String> seededDimensions = jdbcTemplate.query(
                 "select dimension_code from evaluation_dimension where dimension_code in ('TEACHING_DESIGN', 'TEACHING_EFFECTIVENESS') order by dimension_code",
                 (rs, rowNum) -> rs.getString("dimension_code"));
 
-        assertThat(userCount).isEqualTo(4);
+        assertThat(userCount).isEqualTo(5);
+        assertThat(memberCount).isEqualTo(3);
         assertThat(dimensionCount).isEqualTo(5);
-        assertThat(seededUsers).containsExactly("leader01:LEADER", "member01:MEMBER");
+        assertThat(seededUsers).containsExactly(
+                "admin01:ADMIN",
+                "leader01:LEADER",
+                "member01:MEMBER",
+                "member02:MEMBER",
+                "member03:MEMBER");
         assertThat(seededDimensions).containsExactly("TEACHING_DESIGN", "TEACHING_EFFECTIVENESS");
     }
 }
